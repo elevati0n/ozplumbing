dfb89c1623deb277dd60150dce9600bc2187f27a
clean <clean@Johns-MacBook-Air.local>
2016-01-10 00:21:53 -0500

    static pages up, no css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/services'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/testimonials'
+
+  get 'static_pages/faq'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
